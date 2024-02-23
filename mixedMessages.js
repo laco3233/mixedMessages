a68ce5e58974affa69e676f0feec75a7cfcae795
laco3233 <lacombetrevor@gmail.com>
2024-02-23 11:35:37 -0800

    Completed Insult Generator

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,41 +1,19 @@
+
+//object of arrays to store adjectives and nouns for generated insult
 let insult = {
     adjective1: ["bald", "horrible", "terrible", "silly", "dumb"],
     adjective2: ["gawdy", "foolish", "ugly", "high calorie", "grotesque"],
-    noun: ["clown", "sponge", "fool", "MT. Rushmore", "your"],
+    noun: ["clown", "sponge", "fool", "Mount Rushmore", "shell of a human"],
+}
+
+let insultGenerator = () => {
+    let adjective1 = insult.adjective1[Math.floor(Math.random() * insult.adjective1.length)];
+    let adjective2 = insult.adjective2[Math.floor(Math.random() * insult.adjective2.length)];
+    let noun = insult.noun[Math.floor(Math.random() * insult.noun.length)];
+
+    console.log(`You absolute ${adjective1} ${adjective2} ${noun}`);
 }
 
 
-
-
-//console.log('Do you want to hear an insult?');
-
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
-
-// readline.question('What is your name?', name => {
-//     console.log(`Hello, ${name}!`);
-//     readline.close();
-//   });
-
-readline.question(console.log('Do you want to hear an insult?')), answer => {
-    readline.on('line', (input) => {
-        console.log(`Received: ${input}`);
-      }); 
-      answer = input;
-    if (answer === 'yes' || answer === 'y') {
-        console.log(insult);
-    } else if(answer === 'no' || answer === 'n') {
-        console.log('Goodbye!');
-        readline.close();
-    } else {
-        console.log('Please answer yes or no.');
-    }
-    
-}
-
-// readline.on('line', (input) => {
-//     console.log(`Received: ${input}`);
-//   }); 
+insultGenerator();
 
